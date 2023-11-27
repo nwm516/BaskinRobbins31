ae54fb57eef4d0ae0b7af5e5ed80dfe41d84568f
NW Miller <nwm.516@gmail.com>
2023-11-26 20:35:47 -0800

    More files for BaskinRobbins31 project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,42 @@
 package org.example;
 
+import java.util.HashMap;
+
 public class Main {
+
+    private static HashMap<Integer, Integer> baskinCache = new HashMap<Integer, Integer>();
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int finalSolutions = baskin(1,31);
+        System.out.printf("Number of possible outcomes for Baskin Robbins 31 game: %,d%n", finalSolutions);
+
+    }
+
+    private static int baskin(int currentNumber, int maxValue) {
+        // Establishing possible outcomes integer
+        int outcomes = 0;
+
+        // Checking to see if previously calculated and in cache
+        if (baskinCache.containsKey(currentNumber)) {
+            return baskinCache.get(currentNumber);
+        }
+        // Base case
+        if (currentNumber == maxValue) {
+            outcomes = 1;
+        }
+
+        /* Loop explores possible outcomes for numbers to be added,
+         * adding them to solutions of potential next steps in game */
+        for (int addedNum = 1; addedNum <= 3; addedNum++) {
+            int next = currentNumber + addedNum;
+
+            if (next <= maxValue) {
+                outcomes += baskin(next, maxValue);
+            }
+        }
+        // Storing possible outcomes in baskinCache with currentNumber as key and outcomes as value
+        baskinCache.put(currentNumber, outcomes);
+
+        return outcomes;
     }
 }
